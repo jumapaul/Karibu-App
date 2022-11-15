11e06b759b4c2211c3e2ad04377d4e1ece46e637
jumapaul <pauljuma736@gmail.com>
2022-11-15 14:13:40 +0300

    "working on the home page"

--- app/src/main/java/com/pauljuma/karibuapp/MainActivity.kt
+++ app/src/main/java/com/pauljuma/karibuapp/MainActivity.kt
@@ -2,12 +2,26 @@ package com.pauljuma.karibuapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.pauljuma.karibuapp.database.AppDatabase
+import com.pauljuma.karibuapp.repository.KaribuRepository
+import com.pauljuma.karibuapp.viewmodel.KaribuViewModel
+import com.pauljuma.karibuapp.viewmodel.KaribuViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var karibuViewModel: KaribuViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
+
+        val karibuRepository = KaribuRepository(AppDatabase(this))
+        val karibuViewModelFactory = KaribuViewModelFactory(karibuRepository)
+
+        karibuViewModel =
+            ViewModelProvider(this, karibuViewModelFactory).get(KaribuViewModel::class.java)
     }
 }
--- app/src/main/java/com/pauljuma/karibuapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/pauljuma/karibuapp/fragments/HomeFragment.kt
@@ -5,15 +5,70 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.pauljuma.karibuapp.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.pauljuma.karibuapp.MainActivity
+import com.pauljuma.karibuapp.adapters.FavoriteMealsAdapter
+import com.pauljuma.karibuapp.adapters.FeaturedPartnersAdapter
+import com.pauljuma.karibuapp.databinding.FragmentHomeBinding
+import com.pauljuma.karibuapp.viewmodel.KaribuViewModel
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+
+    private val featuredPartnerAdapter: FeaturedPartnersAdapter by lazy { FeaturedPartnersAdapter() }
+    private val favoriteMealsAdapter: FavoriteMealsAdapter by lazy { FavoriteMealsAdapter() }
+    private lateinit var viewBinding: FragmentHomeBinding
+    lateinit var karibuViewModel: KaribuViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+
+        viewBinding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        karibuViewModel = (activity as MainActivity).karibuViewModel
+        observeFeaturedPartners()
+        observeFavoriteMeals()
+
+        return viewBinding.root
+    }
+
+    fun observeFeaturedPartners() {
+        karibuViewModel.featuredPartners.observe(viewLifecycleOwner) {
+            if (it.isNotEmpty()) {
+                featuredPartnerAdapter.addItem(it)
+                setUpFeaturedPartnerRecycleView()
+
+            }
+        }
+    }
+
+    fun observeFavoriteMeals(){
+        karibuViewModel.favoriteMeals.observe(viewLifecycleOwner){
+            if (it.isNotEmpty()){
+                favoriteMealsAdapter.addFavorites(it)
+                setUpFavoriteMealsRecycleview()
+            }
+        }
+    }
+
+    fun setUpFeaturedPartnerRecycleView() {
+        rvPopularDishes.apply {
+            hasFixedSize()
+            adapter = featuredPartnerAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        }
+    }
+
+    fun setUpFavoriteMealsRecycleview() {
+        rvFavoriteMeals.apply {
+            hasFixedSize()
+            adapter = favoriteMealsAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        }
     }
 }
